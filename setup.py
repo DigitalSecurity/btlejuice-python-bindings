36a62c053b9ae7f03215a514e7f7dbc80cc3f64c
Damien Cauquil <damien.cauquil@digitalsecurity.fr>
2016-10-19 11:14:23 +0200

    Added the BtleJuice Python bindings URL

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email='damien.cauquil@digitalsecurity.fr',
     license='MIT',
     packages=['btlejuice','btlejuice.socketIO_client'],
+    url='https://github.com/DigitalSecurity/btlejuice-python-bindings',
     install_requires=[
         'six',
         'websocket'
